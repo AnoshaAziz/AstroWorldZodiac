9d691295f127620dca5f49939cf7e9e30922e3f1
Anosha Aziz <anoshaaziz@outlook.com>
2020-05-31 04:04:00 +0500

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AstroWorldZodiac.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
